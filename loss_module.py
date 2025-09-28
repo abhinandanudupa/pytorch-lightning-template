1977ca017071413f365bf2d187ecae97a5145661
Abhinandan Udupa <67597710+abhinandanudupa@users.noreply.github.com>
2025-09-28 16:36:09 +0530

    [template] Change losses dict to list and add type annotations

--- loss_module.py
+++ loss_module.py
@@ -5,7 +5,7 @@ import torch
 loss_clses: Dict[str, Union[torch.nn.Module, Any]] = {}
 
 class LossModule(torch.nn.Module):
-    def __init__(self, losses: Dict[str, Dict[str, Any]]):
+    def __init__(self, losses: Union[List[Dict[str, Dict[str, Any]]], List[Dict[str, Dict[str, Any]]]]):
         self.losses = []
         for name, config in losses:
             wt = config["weight"]
@@ -18,7 +18,7 @@ class LossModule(torch.nn.Module):
             })
     
     def forward(self, x, y):
-        loss = {"total": torch.Tensor(0.0, device=x.device)}
+        loss: Dict[str, torch.Tensor] = {"total": torch.Tensor(0.0, device=x.device)}
         for loss_fn in self.losses:
             loss[name] = loss_fn["fn"](x, y)
             loss["total"] += (loss[name] * loss_fn["weight"])
