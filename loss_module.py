e4f4769e9dab7414bf9655d35703e43fc3530c83
Abhinandan Udupa <67597710+abhinandanudupa@users.noreply.github.com>
2025-09-28 16:16:59 +0530

    [template] Add loss module

---
+++ loss_module.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict, List, Tuple, Union
+
+import torch
+
+loss_clses: Dict[str, Union[torch.nn.Module, Any]] = {}
+
+class LossModule(torch.nn.Module):
+    def __init__(self, losses: Dict[str, Dict[str, Any]]):
+        self.losses = []
+        for name, config in losses:
+            wt = config["weight"]
+            params = config["params"]
+            fn = loss_clses["name"](**params)
+            self.losses.append({
+                "name": name,
+                "weight": wt,
+                "fn": fn,
+            })
+    
+    def forward(self, x, y):
+        loss = {"total": torch.Tensor(0.0, device=x.device)}
+        for loss_fn in self.losses:
+            loss[name] = loss_fn["fn"](x, y)
+            loss["total"] += (loss[name] * loss_fn["weight"])
+
+        return loss
+
