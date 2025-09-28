e53d7027c82c1819ec1675468e61c773007104b7
Abhinandan Udupa <67597710+abhinandanudupa@users.noreply.github.com>
2025-09-28 18:11:43 +0530

    [template] Fix type declaration and format files

--- main.py
+++ main.py
@@ -10,15 +10,31 @@ from module import LightningWrapper
 if __name__ == "__main__":
     model = Model()
     exp_name = "exp"
-    loss_fn = LossModule(losses=[{"mse": {"params": {"reduction": "mean"}, "weight": 1.0}}])
+    loss_fn = LossModule(
+        losses=[{"mse": {"params": {"reduction": "mean"}, "weight": 1.0}}]
+    )
     lmod = LightningWrapper(model=model, loss_fn=loss_fn)
-    logger = lightning.pytorch.loggers.TensorBoardLogger(f"logs/{exp_name}", name=exp_name)
-    mc = lightning.pytorch.callbacks.ModelChecpoint(monitor="valid/loss", dirpath=f"checkpoints/{exp_name}", filename="checkpoint-{epoch:02d}-{val_acc:.2f}", save_top_k=-1, mode="min", verbose=True)
-    es = lightning.pytorch.callbacks.EarlyStopping(monitor="valid/loss", patience=3, verbose=True, mode="min")
+    logger = lightning.pytorch.loggers.TensorBoardLogger(
+        f"logs/{exp_name}", name=exp_name
+    )
+    mc = lightning.pytorch.callbacks.ModelChecpoint(
+        monitor="valid/loss",
+        dirpath=f"checkpoints/{exp_name}",
+        filename="checkpoint-{epoch:02d}-{val_acc:.2f}",
+        save_top_k=-1,
+        mode="min",
+        verbose=True,
+    )
+    es = lightning.pytorch.callbacks.EarlyStopping(
+        monitor="valid/loss", patience=3, verbose=True, mode="min"
+    )
     trainer = lightning.Trainer(
         max_epochs=20,
         accelerator="auto",
-        callbacks=[mc, es],
+        callbacks=[
+            mc,
+            es,
+        ],
         logger=logger,
         log_every_n_steps=1,
         enable_progress_bar=True,
@@ -34,12 +50,17 @@ if __name__ == "__main__":
         benchmark=True,
         detect_anomaly=False,
     )
-    train_ds = torchvision.datasets.datasets.MNIST("data", train=True, download=True, transform=transform)
-    val_ds = torchvision.datasets.MNIST("data", train=False, download=True, transform=transform)
-    
-    train_loader = torch.utils.data.DataLoader(train_ds, batch_size=64, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_ds, batch_size=64)
+    train_ds = torchvision.datasets.datasets.MNIST(
+        "data", train=True, download=True, transform=transform
+    )
+    val_ds = torchvision.datasets.MNIST(
+        "data", train=False, download=True, transform=transform
+    )
+    batch_size = 64
+
+    train_loader = torch.utils.data.DataLoader(
+        train_ds, batch_size=batch_size, shuffle=True
+    )
+    val_loader = torch.utils.data.DataLoader(val_ds, batch_size=batch_size)
 
     trainer.fit(lmod, train_loader, valid_loader)
-
-
--- module.py
+++ module.py
@@ -1,11 +1,17 @@
 from typing import Dict, List, Tuple
 
 import torch
-import lightning 
+import lightning
 
 
 class LightningWrapper(lightning.LightningModule):
-    def __init__(self, model: torch.nn.Module, loss_fn: torch.nn.Module, lr: float=1e-3, **kwargs):
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        loss_fn: torch.nn.Module,
+        lr: float = 1e-3,
+        **kwargs,
+    ):
         super.__init__()
         self.save_hyperparameters()
         self.model: torch.nn.Module = model
@@ -13,22 +19,33 @@ class LightningWrapper(lightning.LightningModule):
 
     def forward(self, *args, **kwargs):
         out: torch.Tensor = self.model(*args, **kwargs)
-        
+
         return out
 
     def training_step(self, batch, batch_idx):
-        x: torch.Tensor, y: torch.Tensor = batch
+        x: torch.Tensor
+        y: torch.Tensor
+        x, y = batch
         out: torch.Tensor = self.model(x)
         loss: Dict[str, torch.Tensor] = self.loss_fn(out, y)
         self.log("train/loss", loss["total"])
-        
+
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
-        x: torch.Tensor, y: torch.Tensor = batch
+        x: torch.Tensor
+        y: torch.Tensor
+        x, y = batch
         out: torch.Tensor = self.model(x)
         loss: Dict[str, torch.Tensor] = self.loss_fn(out, y)
-        self.log("valid/loss", loss["total"], prog_bar=True,logger=True, on_epoch=True, sync_dist=True)
+        self.log(
+            "valid/loss",
+            loss["total"],
+            prog_bar=True,
+            logger=True,
+            on_epoch=True,
+            sync_dist=True,
+        )
 
         return loss
 
@@ -46,10 +63,8 @@ class LightningWrapper(lightning.LightningModule):
                 },
             },
         ]
-        
+
     def on_train_epoch_end(self):
         for i, opt in enumerate(self.optimizers()):
             lr = opt.param_groups[0]["lr"]
             self.log(f"lr_opt_{i}", lr)
-
-
